51514834bc18cbbede7598dd3dae78526302ee4d
GeorgeOtegbeye <otegbeyegeorge1@gmail.com>
2024-08-02 16:22:43 +0100

    fixed a bug in the count value

--- index.js
+++ index.js
@@ -10,21 +10,27 @@ function increment() {
 function save() {
   let countStr = count + " - ";
   saveEl.textContent += countStr;
-  // count = 0;
   // I used textContent instead of innerText because innerText ignores the styling.
+  countEl.textContent = 0;
+  // sets value of count to 0
+  count = 0;
+  // makes sure the count doesn't continue from the last value
 }
 
 function double() {
   count += 2;
   countEl.innerText = count;
+  // doubles the count
 }
 
 function triple() {
   count += 3;
   countEl.innerText = count;
 }
+// triples the count
 
 function reset() {
   countEl.innerText = 0;
-  saveEl.textContent = 'Previous entries: ';
+  saveEl.textContent = "Previous entries: ";
+  // resets the count and previous entries to zero
 }
