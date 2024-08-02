7c675b49412f70fe3127615e9339f8ce10a0a59a
GeorgeOtegbeye <otegbeyegeorge1@gmail.com>
2024-08-02 15:27:37 +0100

    Created buttons and added background image

---
+++ index.js
@@ -0,0 +1,12 @@
+countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+  console.log(count);
+}
+
+function save() {
+  
+}
