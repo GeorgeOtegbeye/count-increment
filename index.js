a2d9093200675abfeaa71fdfacb6b8c28e046ddf
GeorgeOtegbeye <otegbeyegeorge1@gmail.com>
2024-08-02 15:48:05 +0100

    added functionality to all buttons

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-countEl = document.getElementById("count-el");
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 let count = 0;
 
 function increment() {
   count += 1;
   countEl.innerText = count;
-  console.log(count);
 }
 
 function save() {
-  
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  // count = 0;
+  // I used textContent instead of innerText because innerText ignores the styling.
+}
+
+function double() {
+  count += 2;
+  countEl.innerText = count;
+}
+
+function triple() {
+  count += 3;
+  countEl.innerText = count;
+}
+
+function reset() {
+  countEl.innerText = 0;
+  saveEl.textContent = 'Previous entries: ';
 }
